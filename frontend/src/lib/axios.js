37734acf2ebe60738b0f4c2f20a8bd7305dbf9d5
oNk2r <ort.200516@gmail.com>
2026-02-25 02:58:39 +0530

    axios fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "production" ?  "http://localhost:3001/api" : "/api";
+// in production, there's no localhost so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
